7013fe25fcfc93acf374b53e0f2e4a00db719099
Maxwell Chrysler <maxwellchrysler@hotmail.com>
2023-05-05 22:02:03 -0500

    Created UI

---
+++ client.js
@@ -0,0 +1,46 @@
+console.log('hi');
+
+$(document).ready(onReady);
+
+function onReady(){
+//handleSubmit on click button stuff
+
+//change the onclick to submit if things are in the html forms 
+
+
+
+function handleSubmit(event){
+    console.log('These are things to do');
+    event.preventDefault(); 
+
+    let firstName = $('#first-name').val(); //
+    let lastName = $('#last-name').val();
+    let idNumber = $('#id-number').val();
+    let jobTitle = $('#job-title').val();
+    let annualSalary = $('#annual-salary').val();
+
+
+
+}
+/*
+function 
+    let employeeInfo=[];
+    function employeeObject(){
+        firstName:,
+        lastName:,
+        idNumber:,
+        jobTitle:,
+        annualSalary:
+
+    }
+
+
+*/
+
+}
+
+
+function handleSubmit(event){
+
+
+}
