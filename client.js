0affc70499954ee2cb109fa2da20ec8a1868e42b
Maxwell Chrysler <maxwellchrysler@hotmail.com>
2023-05-09 11:28:04 -0500

    added visual changes and clearing input fields

--- client.js
+++ client.js
@@ -2,16 +2,21 @@ console.log('hi');
 
 $(document).ready(onReady);
 
-let monthlyExpense =0;
+let monthlyExpense = 0;
+
+
+
 
 function onReady(){
 
     $('#form-input').on('submit', submitHandle);
     $('#table-input').on('click', '.delete-btn', handleDelete)
+
+}
 //handleSubmit on click button stuff
 
 //change the onclick to submit if things are in the html forms      
-}
+
 
 //When adding the annual salaries together make sure to divide by 12 and then to round the averages so it looks good 
 //use like .math or something
@@ -22,7 +27,7 @@ function submitHandle(event){
 
     let firstName = $('#first-name').val(); //
     let lastName = $('#last-name').val();
-    let idNumber = $('#ID').val();
+    let idNumber = +$('#ID').val(); // putting + before $ will make this a number
     let jobTitle = $('#job-title').val();
     let annualSalary = Number($('#annual-salary').val());
     updateTotal(annualSalary)
@@ -35,12 +40,21 @@ function submitHandle(event){
     <td>$${annualSalary}</td>
     <td><button class = "delete-btn">delete</button></td>
     </tr>`);
+
+
+    //$('input').val('') In this program we can use this to clear all the inputs
+    $('#first-name').val(''); // This is used to clear the input field
+    $('#last-name').val('');
+    +$('#ID').val(''); 
+    $('#job-title').val('');
+    Number($('#annual-salary').val(''));
+
 }
 
 function updateTotal(money){
- monthlyExpense += money/12;
+ monthlyExpense +=Math.round(money/12);
  console.log(monthlyExpense);
-$('#monthlyTotal').text(monthlyExpense);
+$('#monthly-total').text(monthlyExpense);
 
 if(monthlyExpense>20000){
 $('footer').css('background-color','red');
@@ -48,6 +62,10 @@ $('footer').css('background-color','red');
     }
 }
 
+
+
 function handleDelete() {
+
     $(this).parent().parent().remove();
+
 }
